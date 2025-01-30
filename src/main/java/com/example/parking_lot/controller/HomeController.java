043e4fb8777b6afa86964a7926b0961c922e6c26
Smit Khobragade <smitkbgd@gmail.com>
2025-01-30 19:57:51 +0530

    Initailized Spring Project

---
+++ src/main/java/com/example/parking_lot/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.parking_lot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+    @GetMapping
+    public String home(){
+        return "Welcome to Parking Lot System!";
+    }
+}
